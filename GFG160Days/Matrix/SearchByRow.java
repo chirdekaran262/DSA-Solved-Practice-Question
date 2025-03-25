ca3395924a8e74b0027517532c14f0c1cfda56f2
chirdekaran262 <chirdekaran262@gmail.com>
2025-03-25 16:53:11 +0530

    Add the Serching problem

---
+++ GFG160Days/Matrix/SearchByRow.java
@@ -0,0 +1,38 @@
+class SearchByRow {
+    public static boolean binarySearch(int mat[],int x){
+        int low=0,high=mat.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(mat[mid]==x){
+                return true;
+            }
+            else if(mat[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+    public static boolean matSearch(int mat[][], int x) {
+        // your code here
+        for(int i=0;i<mat.length;i++){
+            if(binarySearch(mat[i],x)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[][] mat = {{3, 30, 38},
+                       {20, 52, 54},
+					   {35, 60, 69}};
+        int x = 35;
+        
+        if(matSearch(mat, x)) 
+            System.out.println("true");
+        else 
+            System.out.println("false");
+    }
+}
