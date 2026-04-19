b333f6b866ab7117b39e575f0cc8eb6d7b90dfb2
chirdekaran262 <chirdekaran262@gmail.com>
2026-04-19 11:40:52 +0530

    checkpower

---
+++ POTD/CheckPower.java
@@ -0,0 +1,17 @@
+class CheckPower {
+    public boolean isPower(int x, int y) {
+        // code here
+        if(y==1) return true;
+        if(x==0) return false;
+        if(x==1) return y==1;
+        
+        while(y%x==0){
+            y=y/x;
+        }
+        return y==1;
+    }
+    public static void main(String[] args) {
+        CheckPower cp = new CheckPower();
+        System.out.println(cp.isPower(2, 8)); // true
+    }
+}
