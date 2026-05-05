22cf14b367ac07d01f41fbcde5dbe439c46e3bb9
chirdekaran262 <chirdekaran262@gmail.com>
2026-05-05 09:41:04 +0530

    Sum of XOR of all pairs

---
+++ POTD/SumofXORofallpairs.java
@@ -0,0 +1,21 @@
+class SumofXORofallpairs{
+    public long sumXOR(int[] arr) {
+        // code here
+        int n=arr.length;
+        long total=0;
+        for(int i=0;i<32;i++){
+            long count1=0;
+            for(int num:arr){
+                if((num&(1<<i))!=0) count1++;
+            }
+            long count0=n-count1;
+            total+=count1*count0*(1L<<i);
+        }
+        return total;
+    }
+    public static void main(String[] args) {
+        SumofXORofallpairs obj = new SumofXORofallpairs();
+        int[] arr = {1, 2, 3};
+        System.out.println(obj.sumXOR(arr));
+    }
+}
